37ba8f42fb4d98ef84fc8c67a5eecf82ec9c9bfa
AdamTranquilla <adam.tranquilla@gmail.com>
2021-02-04 23:55:32 +0000

    login functionality added but not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  
   private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+  
+  # Add a before_filter to any controller that you want to secure. 
+  # This will force user's to login before they can see the actions in this controller.
+  # before_filter :authorize
 
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
